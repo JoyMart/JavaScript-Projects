39f2a6e8ec9a9d4d099b9689e7a8747a47c730e0
Joy <JoyMart@users.noreply.github.com>
2020-07-27 13:01:28 -0500

    hw
    add elements to code, hw assignment

--- Basic_JavaScript_Proj/JS/Basic_JavaScript_1.js
+++ Basic_JavaScript_Proj/JS/Basic_JavaScript_1.js
@@ -1,37 +1,36 @@
-window.alert('Hello, World!')
-document.write('Hello World!')
+window.alert('Hello, World!') //assigning a window pop-up
+document.write('Hello World!')// printing pop-up
 
-var A = "I like apples";
-var A =A.fontcolor('red');
-document.write(A);
+var A = "I like apples"; //assigning variable
+var A =A.fontcolor('red');//assigning color to variable
+document.write(A);//printing variable
 
-document.write("<br><br>");
+document.write("<br><br>"); //two spaces
 
 document.write("Susan scoffed, \"you really want to do this right now?\"<br> Silvia grabbed the purse\"you bet your ass I do!\"");
 
-document.write("<br><br><br>");
+document.write("<br><br><br>"); //three spaces(returns)
 
 document.write("Sunday Morning rain is falling"
 +" steal some covers, share some skin"
 + " clouds are shrouding us in moments unforgetable"
 +" you twist to fit the mold that I am in..."
-+"- Maroon5");
++"- Maroon5"); //writing a string
 
-document.write('<br><br><br>');
+document.write('<br><br><br>'); //three sapces
 
-var B = " this is "+" concatenated"
-var B =B.fontcolor('green');
-document.write(B);
+var B = " this is "+" concatenated" //assigning a variable
+var B =B.fontcolor('green'); //assigning a color to said variable
+document.write(B); //printing variable
 
-document.write('<br><br><br>');
+document.write('<br><br><br>'); //three spaces
 
 var family = 'Marts', Husband = 'Skyler', Wife='Joy', Pet='Paxton';
-var family =family.fontcolor('blue');
-var Pet=Pet.fontcolor("maroon");
-document.write(Pet)
+var family =family.fontcolor('blue'); //adding color to variables
+var Pet=Pet.fontcolor("maroon"); //adding color to variables
+document.write(Pet) //writing variable
 
-document.write('<br><br><br>');
+document.write('<br><br><br>'); //three spaces
 
-document.write(3+3);
-
+document.write(3+3); // writing an expression
 
---
+++ Basic_JavaScript_Proj/JS/Basic_JavaScript_2.js
@@ -0,0 +1,17 @@
+function colors() {
+    var str = "This text is pink";
+    var result = str.fontcolor("pink");
+    document.getElementById("pink_Text").innerHTML = result;
+}
+
+document.body.style.backgroundColor = "lightblue";
+
+
+
+
+function StarWars() {
+    var quote = "Why, hello there";
+    quote += "!";
+    document.getElementById("hello_there").innerHTML = quote;
+}
+
