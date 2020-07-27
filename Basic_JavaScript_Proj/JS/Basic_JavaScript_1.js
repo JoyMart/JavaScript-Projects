f31055a5646ddbfc80230fe668f2df71672895e4
Joy <JoyMart@users.noreply.github.com>
2020-07-27 11:00:49 -0500

    push first doc
    create document, add hw requirements

---
+++ Basic_JavaScript_Proj/JS/Basic_JavaScript_1.js
@@ -0,0 +1,37 @@
+window.alert('Hello, World!')
+document.write('Hello World!')
+
+var A = "I like apples";
+var A =A.fontcolor('red');
+document.write(A);
+
+document.write("<br><br>");
+
+document.write("Susan scoffed, \"you really want to do this right now?\"<br> Silvia grabbed the purse\"you bet your ass I do!\"");
+
+document.write("<br><br><br>");
+
+document.write("Sunday Morning rain is falling"
++" steal some covers, share some skin"
++ " clouds are shrouding us in moments unforgetable"
++" you twist to fit the mold that I am in..."
++"- Maroon5");
+
+document.write('<br><br><br>');
+
+var B = " this is "+" concatenated"
+var B =B.fontcolor('green');
+document.write(B);
+
+document.write('<br><br><br>');
+
+var family = 'Marts', Husband = 'Skyler', Wife='Joy', Pet='Paxton';
+var family =family.fontcolor('blue');
+var Pet=Pet.fontcolor("maroon");
+document.write(Pet)
+
+document.write('<br><br><br>');
+
+document.write(3+3);
+
+
